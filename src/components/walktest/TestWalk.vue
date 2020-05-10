43ffc51721a6de2a9d05eccf06547b072d26770b
Dario Salvi <dariosalvi78@gmail.com>
2020-05-10 23:31:10 +0200

    added walking guy

--- src/components/walktest/TestWalk.vue
+++ src/components/walktest/TestWalk.vue
@@ -1,32 +1,39 @@
 <template id="testWalk">
   <v-ons-page>
-    <p style="text-align: center">
-      adasdads
-    </p>
-    <div style="text-align: center">
-      <v-ons-button @click="endTest">
-        End
-      </v-ons-button>
-    </div>
-    <div style="text-align: center">
-      <v-ons-button @click="cancelTest">
-        Cancel
-      </v-ons-button>
+    <div class="content">
+      <walking-man ref="walkingMan"/>
+      <div style="text-align: center">
+        <v-ons-button @click="endTest">
+          End
+        </v-ons-button>
+      </div>
+      <div style="text-align: center">
+        <v-ons-button @click="cancelTest">
+          Cancel
+        </v-ons-button>
+      </div>
     </div>
   </v-ons-page>
 </template>
 
 <script>
 import testCompleted from './TestCompleted'
+import WalkingMan from './WalkingMan'
 
 export default {
   name: 'TestWalkPage',
+  components: { WalkingMan },
+  mounted () {
+
+    this.$refs.walkingMan.play()
+  },
   methods: {
     async endTest () {
+      this.$refs.walkingMan.stop()
       this.$emit('push-page', testCompleted)
     },
     async cancelTest () {
-      console.log('test cancelled')
+      this.$refs.walkingMan.stop()
       this.$parent.popPage()
     }
   }
