3b0cec573fe911d428c9a80e5f4a5fafa60f1cbe
Dario Salvi <dariosalvi78@gmail.com>
2022-03-14 17:16:39 +0100

    added minutes and meters back

--- src/i18n/it/history.js
+++ src/i18n/it/history.js
@@ -4,7 +4,9 @@ export default {
     date: 'Data del test',
     duration: 'Durata',
     distance: 'Distanza',
-    steps: 'Passi'
+    steps: 'Passi',
+    minutes: 'minuti',
+    meters: 'metri'
   },
   shareTopic: 'Cronologia dei test del cammino',
   shareError: 'Impossibile condividere la cronologia'
