78ad49c5a6d58e1c187d561978717ca1ba71b1ef
dariosalvi78 <dariosalvi78@gmail.com>
2026-02-16 20:20:57 +0100

    dzongkha translation

---
+++ src/i18n/dz/about.js
@@ -0,0 +1,16 @@
+import smwt from './smwt'
+import license from './license'
+import acks from './acks'
+import privacyPolicy from './privacyPolicy'
+
+export default {
+  title: 'སྐོར',
+  smwtTitle: 'སྐར་མ་ ༦ གི་གོམ་པ་རྒྱག་པའི་བརྟག་དཔྱད།',
+  acksTitle: 'ངོས་ལེན་ཚུ་',
+  privacyPolicyTitle: 'སྒེར་དོན་སྲིད་བྱུས།',
+  licenseTitle: 'ཆོག་ཐམ་',
+  smwt: smwt,
+  license: license,
+  acks: acks,
+  privacyPolicy: privacyPolicy
+}
---
+++ src/i18n/dz/en.js
@@ -0,0 +1,17 @@
+import home from './home'
+import history from './history'
+import about from './about'
+import walk from './walk'
+
+export default {
+  menu: {
+    home: 'ཁྱིམ་',
+    walk: 'འགྲོ',
+    history: 'ལོ་རྒྱུས།',
+    about: 'སྐོར།'
+  },
+  home,
+  history,
+  about,
+  walk
+}
