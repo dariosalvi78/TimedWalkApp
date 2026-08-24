939fb3c0d7febbfc2e69f61f4efdaa6d1eccd9f9
dariosalvi78 <dariosalvi78@gmail.com>
2026-08-24 18:32:54 +0200

    adding rotating logs

--- servers/complete/api/services/logger.js
+++ servers/complete/api/services/logger.js
@@ -1,10 +1,21 @@
 import winston from 'winston'
+import { createStream } from "rotating-file-stream"
+import path from 'node:path'
+
+const LOG_FILE_PATH = process.env.LOG_FILE_PATH || 'log.log'
+const LOG_MAX_SIZE = process.env.LOG_MAX_SIZE || '10M' // 10 Megabytes
+
+const rotatingStream = createStream(LOG_FILE_PATH, {
+  size: LOG_MAX_SIZE,      // rotate every X MegaBytes written
+  // interval: '1d',   // rotate daily
+  compress: 'gzip', // compress rotated files
+})
 
 const logger = winston.createLogger({
   level: process.env.LOGLEVEL || 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: 'logs.log' }),
+    new winston.transports.Stream({ stream: rotatingStream }),
   ]
 })
 
